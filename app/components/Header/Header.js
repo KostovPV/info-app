e1c2ed6c6d049f8541fe3548948308491c60e320
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-11-18 21:54:47 +0200

    Header and layout edited

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,23 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import classes from './Header.module.css';
 
 
 export default function Header() {
     return <>
-        <ul>
-            <li>
-                <Link href="/">
-                    {/* <Image  alt="A plate with food on it" priority /> */}
-                    Marine info
-                </Link>
-            </li>
-            <li>
-                <Link href="/jobs">Browse all jobs</Link>
-            </li>
-            <li>
-                <Link href="/blogs">Browse all blogs</Link>
-            </li>
-        </ul>
+        <section className={classes.navigation}>
+            <ul className={classes.list}>
+                <li>
+                    <Link href="/">
+                        {/* <Image  alt="A plate with food on it" priority /> */}
+                        Marine info
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/jobs">Browse all jobs</Link>
+                </li>
+                <li>
+                    <Link href="/blogs">Browse all blogs</Link>
+                </li>
+            </ul>
+
+        </section>
+
     </>
 }
--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (<>
     <div className={styles.page}>
+      <h1>All information for Marine proffecionals in one place</h1>
       <main className={styles.main}>
-        <p>Home page</p>
 
       </main>
 
