ce79ecec5b9aec345dad514b02e3a23beee36de6
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-11-20 12:26:29 +0200

    page.js edited

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (<>
     <div className={styles.page}>
-      <h1>All information for Marine proffecionals in one place</h1>
+      <h1 >All information for Marine proffecionals in one place</h1>
       <main className={styles.main}>
-
+        <div className={styles.jobContainer}>
+          <div>Find the dreamed job at sea</div>
+          <button>
+            <Link href='/jobs'>Browse jobs</Link>
+          </button>
+          
+        </div>
       </main>
 
     </div>
