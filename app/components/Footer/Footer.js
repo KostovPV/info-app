9f2c930b8ae2c48bcc5e34e4c5925848372a1f93
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-11-20 15:17:13 +0200

    Footer edited

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -4,7 +4,18 @@ import styles from './Footer.module.css';
 export default function Footer() {
     return (
         <div className={styles.footer}>
-         Footer
+            <div className={styles.footerMain}>
+                <ul>
+                    <li>Job list</li>
+                    <li>Blogs</li>
+                    <li>General info</li>
+                    <li>Socials</li>
+                    <li>Contacts</li>
+                </ul>
+            </div>
+            <div className={styles.footerText}>
+                Marine info - all the information for marine proffecionals in one place
+            </div>
         </div>
     )
 }
