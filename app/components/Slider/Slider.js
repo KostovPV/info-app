3b276dfda81f776f663cb192637c616a8c438514
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-11-21 21:37:10 +0200

    Slider component created

---
+++ app/components/Slider/Slider.js
@@ -0,0 +1,17 @@
+import classes from './Slider.module.css';
+
+export default function Slider() {
+  return (
+    <section className={classes.sliderContainer}>
+        <div>
+            Hot Jobs
+        </div>
+        <div>
+            Latest news
+        </div>
+        <div>
+            Latest blogs
+        </div>
+    </section>
+  )
+}
--- app/jobs/page.js
+++ app/jobs/page.js
@@ -1,12 +1,19 @@
 
-// import classes from './page.module.css';
+import classes from './page.module.css';
 
 export default function JobsPage() {
   return (
-    <>
-      <p>
-        Job page here
-      </p>
-    </>
+    <section className={classes.jobsContainer}>
+      <h2>Browse all gobs to find your perfect place</h2>
+      <div className={classes.jobsWrapper}>
+        <div className={classes.asside}>
+          Filter criteria here
+        </div>
+        <div className={classes.jobList}>
+          All jobs here
+        </div>
+
+      </div>
+    </section>
   );
 }
