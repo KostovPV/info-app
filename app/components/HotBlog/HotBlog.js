de4bed9bc1692561d99900b61d052cfe880a8559
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-12-16 16:13:32 +0200

    Edited Slider and HotBlog components

--- app/components/HotBlog/HotBlog.js
+++ app/components/HotBlog/HotBlog.js
@@ -4,8 +4,13 @@ import classes from "./HotBlog.module.css";
 
 export default function HotBlog() {
   return (
-    <div>
-        <BlogCard />
+    <div className={classes.hotBlogsContainer} >
+      <h2>Latest blogs</h2>
+      <div className={classes.hotBlogsList}>
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      </div>
     </div>
   )
 }
